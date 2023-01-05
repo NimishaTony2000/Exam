7f0da7b0b570721add4a9ee2c784a80397b7acef
NimishaTony2000 <95517102+NimishaTony2000@users.noreply.github.com>
2023-01-05 12:31:42 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.sjcet.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void open(MenuItem item) {
+        Toast.makeText(this, "Open activated", Toast.LENGTH_SHORT).show();
+    }
+
+    public void share(MenuItem item) {
+        Toast.makeText(this, "Share activated", Toast.LENGTH_SHORT).show();
+    }
+
+    public void Delete(MenuItem item) {
+        Toast.makeText(this, "delete activated", Toast.LENGTH_SHORT).show();
+    }
+
+    public void sech(MenuItem item) {
+        Toast.makeText(this, "Search activated", Toast.LENGTH_SHORT).show();
+    }
+
+    public void settng(MenuItem item) {
+        Toast.makeText(this, "Settings activated", Toast.LENGTH_SHORT).show();
+    }
+}
